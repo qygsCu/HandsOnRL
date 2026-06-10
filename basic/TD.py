0d1028534b00499df8bec78b89cda59bd9a01be6
qygsCu <pengtse.hou@outlook.com>
2026-06-10 15:52:36 +0800

    sarsa and n-step sarsa

---
+++ basic/TD.py
@@ -0,0 +1,164 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm  # tqdm是显示循环进度条的库
+
+
+class CliffWalkingEnv:
+    def __init__(self, ncol, nrow):
+        self.nrow = nrow
+        self.ncol = ncol
+        self.x = 0  # 记录当前智能体位置的横坐标
+        self.y = self.nrow - 1  # 记录当前智能体位置的纵坐标
+
+    def step(self, action):  # 外部调用这个函数来改变当前位置
+        # 4种动作, change[0]:上, change[1]:下, change[2]:左, change[3]:右。坐标系原点(0,0)
+        # 定义在左上角
+        change = [[0, -1], [0, 1], [-1, 0], [1, 0]]
+        self.x = min(self.ncol - 1, max(0, self.x + change[action][0]))
+        self.y = min(self.nrow - 1, max(0, self.y + change[action][1]))
+        next_state = self.y * self.ncol + self.x
+        reward = -1
+        done = False
+        if self.y == self.nrow - 1 and self.x > 0:  # 下一个位置在悬崖或者目标
+            done = True
+            if self.x != self.ncol - 1:
+                reward = -100
+        return next_state, reward, done
+
+    def reset(self):  # 回归初始状态,坐标轴原点在左上角
+        self.x = 0
+        self.y = self.nrow - 1
+        return self.y * self.ncol + self.x
+    
+class Sarsa:
+    def __init__(self, gamma, alpha, env, epsilon, num_actions=4):
+        self.env = env
+        self.num_actions = num_actions
+        self.Q_table = np.zeros([self.env.nrow * self.env.ncol, num_actions])
+        self.gamma = gamma
+        self.alpha = alpha
+        self.epsilon = epsilon
+    
+    def step(self, state):
+        p = np.random.rand()
+        if p < self.epsilon:
+            return np.random.randint(self.num_actions)
+        else:
+            return np.argmax(self.Q_table[state])
+        
+    def update(self, s0, a0, r, s1, a1, done):
+        td_error = r + self.gamma * self.Q_table[s1][a1] * (1 - done) - self.Q_table[s0][a0]
+        self.Q_table[s0][a0] += self.alpha * td_error
+
+ncol = 12
+nrow = 4
+env = CliffWalkingEnv(ncol, nrow)
+np.random.seed(0)
+epsilon = 0.1
+alpha = 0.1
+gamma = 0.9
+agent = Sarsa(gamma, alpha, env, epsilon)
+num_episodes = 500
+
+# for i_episodes in range(num_episodes):
+#     state = env.reset()
+#     action = agent.step(state)
+#     done = False
+#     while not done:
+#         next_state, r, done = env.step(action)
+#         next_action = agent.step(next_state)
+#         agent.update(state, action, r, next_state, next_action, done)
+#         state = next_state
+#         action = next_action
+
+class nstep_Sarsa:
+    """ n步Sarsa算法 """
+    def __init__(self, n, ncol, nrow, epsilon, alpha, gamma, n_action=4):
+        self.Q_table = np.zeros([nrow * ncol, n_action])
+        self.n_action = n_action
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.n = n  # 采用n步Sarsa算法
+        self.state_list = []  # 保存之前的状态
+        self.action_list = []  # 保存之前的动作
+        self.reward_list = []  # 保存之前的奖励
+
+    def take_action(self, state):
+        if np.random.random() < self.epsilon:
+            action = np.random.randint(self.n_action)
+        else:
+            action = np.argmax(self.Q_table[state])
+        return action
+
+    def best_action(self, state):  # 用于打印策略
+        Q_max = np.max(self.Q_table[state])
+        a = [0 for _ in range(self.n_action)]
+        for i in range(self.n_action):
+            if self.Q_table[state, i] == Q_max:
+                a[i] = 1
+        return a
+
+    def update(self, s0, a0, r, s1, a1, done):
+        self.state_list.append(s0)
+        self.action_list.append(a0)
+        self.reward_list.append(r)
+        if len(self.state_list) == self.n:
+            Gt = self.Q_table[s1][a1]
+            for i in reversed(range(self.n)):
+                Gt = self.gamma * Gt + self.reward_list[i]
+                if done and i > 0:
+                    a = self.action_list[i]
+                    s = self.state_list[i]
+                    self.Q_table[s][a] += self.alpha * (Gt - self.Q_table[s][a])
+            s = self.state_list.pop(0)
+            a = self.action_list.pop(0)
+            r = self.reward_list.pop(0)
+            self.Q_table[s][a] += self.alpha * (Gt - self.Q_table[s][a])
+        if done:
+            self.action_list = []
+            self.reward_list = []
+            self.state_list = []
+
+
+np.random.seed(0)
+n_step = 5  # 5步Sarsa算法
+alpha = 0.1
+epsilon = 0.1
+gamma = 0.9
+agent = nstep_Sarsa(n_step, ncol, nrow, epsilon, alpha, gamma)
+num_episodes = 500  # 智能体在环境中运行的序列的数量
+
+return_list = []  # 记录每一条序列的回报
+for i in range(10):  # 显示10个进度条
+    #tqdm的进度条功能
+    with tqdm(total=int(num_episodes / 10), desc='Iteration %d' % i) as pbar:
+        for i_episode in range(int(num_episodes / 10)):  # 每个进度条的序列数
+            episode_return = 0
+            state = env.reset()
+            action = agent.take_action(state)
+            done = False
+            while not done:
+                next_state, reward, done = env.step(action)
+                next_action = agent.take_action(next_state)
+                episode_return += reward  # 这里回报的计算不进行折扣因子衰减
+                agent.update(state, action, reward, next_state, next_action,
+                             done)
+                state = next_state
+                action = next_action
+            return_list.append(episode_return)
+            if (i_episode + 1) % 10 == 0:  # 每10条序列打印一下这10条序列的平均回报
+                pbar.set_postfix({
+                    'episode':
+                    '%d' % (num_episodes / 10 * i + i_episode + 1),
+                    'return':
+                    '%.3f' % np.mean(return_list[-10:])
+                })
+            pbar.update(1)
+
+episodes_list = list(range(len(return_list)))
+plt.plot(episodes_list, return_list)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('5-step Sarsa on {}'.format('Cliff Walking'))
+plt.show()
